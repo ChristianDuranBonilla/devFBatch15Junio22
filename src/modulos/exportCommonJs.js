9ee34a1b16be35afa70b6bc4686c4e89d3e332a9
Daniel Silverio <daniel.silverio@mariachi.io>
2022-06-08 18:57:33 -0500

    Se organizaron de mejor manera los modulos

--- src/modulos/exportCommonJs.js
+++ src/modulos/exportCommonJs.js
@@ -1,15 +1,3 @@
-// Forma de exportar modulos con ESM
-export const error = "error algo salio mal";
-export const datos = {
-  nombre: "Daniel",
-};
-
-export const calculadora = (numero1, numero2) => {
-  return numero1 * numero2;
-};
-
-export const bandera = true;
-
 // Forma de exportar modulos con COMMONJS
 const error = "error algo salio mal";
 const datos = {
--- src/modulos/importESMJS.js
+++ src/modulos/importESMJS.js
@@ -1,11 +1,7 @@
 // forma de importar y exportar con ESM import {} from 'module';
 // No es soportada por NodeJS
 // Se puede observar la importacion en el archivo index.js
-import { error, calculadora, datos, bandera } from "./exportJS.js";
-
-// forma de importar y exportar con COMMON JS const {} = require('module');
-// No es soportada por el navegador
-const { error, calculadora, datos, bandera } = require("./exportJS.js");
+import { error, calculadora, datos, bandera } from "./exportESMJS.js";
 
 console.log("error ", error);
 console.log("calculadora ", calculadora(5, 5));
