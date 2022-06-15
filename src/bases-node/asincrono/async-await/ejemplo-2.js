92b1e54b37deefd427fdfa181b1115d9ffc72f5b
kikemaya <enrique.mg.cd@gmail.com>
2022-06-14 21:36:40 -0500

    get y post del crud v0...

---
+++ src/bases-node/asincrono/async-await/ejemplo-2.js
@@ -0,0 +1,16 @@
+"use strict"
+
+//Async-await, nos permite trabajar respuestas de promesas
+const showData = async () => {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users/')
+    const data = await response.json()
+
+    return console.log(data);
+
+  } catch (error) {
+    return console.error(error);
+  }
+}
+
+showData()
